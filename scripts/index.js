1545d21dbe3bee87c7e1167cf85aa50638654572
Denis <denis1527cherezov@gmail.com>
2023-03-01 16:37:08 +0200

    Correcting Concluding Observations

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let popupProfile = document.querySelector('.popup');
-let formElement = document.querySelector('.profile__edit-button');
+let popupForm = document.querySelector('.popup__form')
+let editButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let nameInput = document.querySelector('.popup__input_data_name');
 let aboutInput = document.querySelector('.popup__input_data_about');
 let profilName = document.querySelector('.profile__name');
 let profilAbout = document.querySelector('.profile__about');
-let popupButton = document.querySelector('.popup__button');
 
 function handleOpenPopupForm() {
-    popupProfile.classList.add('popup_opened');
-    nameInput.value = profilName.textContent;
-    aboutInput.value = profilAbout.textContent;
+  popupProfile.classList.add('popup__opened');
+  nameInput.value = profilName.textContent;
+  aboutInput.value = profilAbout.textContent;
+}
+
+function closePopup(){
+    popupProfile.classList.remove('popup__opened');
   }
-  formElement.addEventListener('click', handleOpenPopupForm)
-  function handleRemovePopupForm(){
-    popupProfile.classList.remove('popup_opened');
-  }
-popupCloseButton.addEventListener('click', handleRemovePopupForm);
-function handleFormSubmit (event) {
-    event.preventDefault()
-    popupProfile.classList.remove('popup_opened');
-    profilName.textContent = nameInput.value;
-    profilAbout.textContent = aboutInput.value;
-};
-popupButton.addEventListener('click', handleFormSubmit);
-formElement.addEventListener('submit', handleFormSubmit); 
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profilName.textContent = nameInput.value;
+  profilAbout.textContent = aboutInput.value;
+  closePopup();
+}
+editButton.addEventListener('click', handleOpenPopupForm);
+popupForm.addEventListener('submit', handleFormSubmit);
+popupCloseButton.addEventListener('click', closePopup);  
