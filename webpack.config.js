4e8b26004176919f7d2ab7a50286c2d60e2b9b65
Denis Cherezov <denis@MacBook-Pro-Denis.local>
2023-05-03 00:33:19 +0200

    Fixing heretical bugs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/pages/index.js' },
+  entry: { main: './scr/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -50,7 +50,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './scr/index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
